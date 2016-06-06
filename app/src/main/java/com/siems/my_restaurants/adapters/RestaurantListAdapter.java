30309191479b5f7ba860c24683af07566247a4e2
Christopher Siems <csiems@gmail.com>
2016-06-06 11:27:23 -0700

    import pictures with picasso

--- app/src/main/java/com/siems/my_restaurants/adapters/RestaurantListAdapter.java
+++ app/src/main/java/com/siems/my_restaurants/adapters/RestaurantListAdapter.java
@@ -10,6 +10,7 @@ import android.widget.TextView;
 
 import com.siems.my_restaurants.R;
 import com.siems.my_restaurants.models.Restaurant;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
@@ -57,6 +58,7 @@ public class RestaurantListAdapter extends RecyclerView.Adapter<RestaurantListAd
         }
 
         public void bindRestaurant(Restaurant restaurant) {
+            Picasso.with(mContext).load(restaurant.getImageUrl()).into(mRestaurantImageView);
             mNameTextView.setText(restaurant.getName());
             mCategoryTextView.setText(restaurant.getCategories().get(0));
             mRatingTextView.setText("Rating: " + restaurant.getRating() + "/5");
