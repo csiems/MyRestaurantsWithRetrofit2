36854d92f6ff910f0152accebfe7d47aae8a4e36
Christopher Siems <csiems@gmail.com>
2016-06-06 10:58:32 -0700

    add api keys to gradle.properties

---
+++ app/src/main/java/com/siems/my_restaurants/Constants.java
@@ -0,0 +1,9 @@
+package com.siems.my_restaurants;
+
+public class Constants {
+    //YELP API KEYS
+    public static final String YELP_CONSUMER_KEY = BuildConfig.YELP_CONSUMER_KEY;
+    public static final String YELP_CONSUMER_SECRET = BuildConfig.YELP_CONSUMER_SECRET;
+    public static final String YELP_TOKEN = BuildConfig.YELP_TOKEN;
+    public static final String YELP_TOKEN_SECRET = BuildConfig.YELP_TOKEN_SECRET;
+}
