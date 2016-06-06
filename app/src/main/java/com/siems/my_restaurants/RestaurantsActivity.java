d62648f58a80a2794e2839337a79a5a126f8c3c2
Christopher Siems <csiems@gmail.com>
2016-06-06 10:24:31 -0700

    add restaurants activity

---
+++ app/src/main/java/com/siems/my_restaurants/RestaurantsActivity.java
@@ -0,0 +1,13 @@
+package com.siems.my_restaurants;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RestaurantsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_restaurants);
+    }
+}
