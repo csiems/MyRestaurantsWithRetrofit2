c8805d732a021c66f27b635738018124faec54b1
Christopher Siems <csiems@gmail.com>
2016-06-06 12:30:10 -0700

    optimize image quality from yelp

--- app/src/main/java/com/siems/my_restaurants/models/Restaurant.java
+++ app/src/main/java/com/siems/my_restaurants/models/Restaurant.java
@@ -25,7 +25,7 @@ public class Restaurant {
         this.mPhone = phone;
         this.mWebsite = website;
         this.mRating = rating;
-        this.mImageUrl = imageUrl;
+        mImageUrl = getLargeImageUrl(imageUrl);
         this.mAddress = address;
         this.mLatitude = latitude;
         this.mLongitude = longitude;
@@ -66,5 +66,10 @@ public class Restaurant {
 
     public ArrayList<String> getCategories() {
         return mCategories;
+    }
+
+    public String getLargeImageUrl(String imageUrl) {
+        String largeImageUrl = imageUrl.substring(0, imageUrl.length() - 6).concat("o.jpg");
+        return largeImageUrl;
     }
 }
