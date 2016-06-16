c6ee465044bcf091e9f52c48dfac7c9109d899cb
Christopher Siems <csiems@gmail.com>
2016-06-15 18:07:24 -0700

    correctly populate detail view from Firebase info

--- app/src/main/java/com/siems/my_restaurants/adapters/FirebaseRestaurantViewHolder.java
+++ app/src/main/java/com/siems/my_restaurants/adapters/FirebaseRestaurantViewHolder.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -54,24 +56,31 @@ public class FirebaseRestaurantViewHolder extends RecyclerView.ViewHolder implem
     @Override
     public void onClick(View view) {
         final ArrayList<Restaurant> restaurants = new ArrayList<>();
-        DatabaseReference ref = FirebaseDatabase.getInstance().getReference(Constants.FIREBASE_CHILD_RESTAURANTS);
-        ref.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    restaurants.add(snapshot.getValue(Restaurant.class));
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            DatabaseReference ref = FirebaseDatabase
+                    .getInstance()
+                    .getReference(user.getUid() + "/" + Constants.FIREBASE_CHILD_RESTAURANTS + "/");
+            ref.addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(DataSnapshot dataSnapshot) {
+                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                        restaurants.add(snapshot.getValue(Restaurant.class));
+                    }
+                    int itemPosition = getLayoutPosition();
+                    Intent intent = new Intent(mContext, RestaurantDetailActivity.class);
+                    intent.putExtra("position", itemPosition + "");
+                    intent.putExtra("restaurants", Parcels.wrap(restaurants));
+                    mContext.startActivity(intent);
                 }
-                int itemPosition = getLayoutPosition();
-                Intent intent = new Intent(mContext, RestaurantDetailActivity.class);
-                intent.putExtra("position", itemPosition + "");
-                intent.putExtra("restaurants", Parcels.wrap(restaurants));
-                mContext.startActivity(intent);
-            }
 
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
+                @Override
+                public void onCancelled(DatabaseError databaseError) {
 
-            }
-        });
+                }
+            });
+
+        }
+
     }
 }
